f109c4207b04020d8ae97b8392c7492510da258e
99Udith <uvisalka@gmail.com>
2025-02-10 21:22:07 +0530

    10/02/2025 Updates

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import './foodDisplay.css'
-import { StoreContext } from '../../context/storeContext'
+import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
@@ -10,7 +10,10 @@ const FoodDisplay = ({category}) => {
     <h2>Top dishes near you</h2>
     <dev className="food-display-list">
         {food_list.map((item,index)=>{
-          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+          if(category==="All" || category===item.category){
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+          }
+          
         })}
         </dev> 
     </div>
